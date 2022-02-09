ebf7d83b98097b9ffd8262ea3c7bf547fdac66be
duque-ricardo <duque.cuerdo@gmail.com>
2022-02-08 21:36:28 -0500

    Mi segundo commit rattatata

--- src/Hand.java
+++ src/Hand.java
@@ -1,2 +1,21 @@
 public class Hand {
+    private int upperBound;
+    private int value;
+
+    public Hand(int upperBound) {
+        this.upperBound = upperBound;
+        this.value = 0;
+    }
+
+    public void advance() {
+        this.value = this.value + 1;
+
+        if (this.value >= upperBound) {
+            this.value = 0;
+        }
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
